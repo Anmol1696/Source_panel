f121f7de11838c4253d453bd77c4f1abf322def8
Anmol1696 <anmol1696@gmail.com>
2016-09-13 04:28:11 +0530

    Adding lines to join successive points

--- plot_functions.py
+++ plot_functions.py
@@ -13,10 +13,12 @@ def plot_C_p_vs_x(C_pi_matrix, panels):
     num_panels = len(panels)
     
     x_i_points = [(panel[0][0] + panel[1][0])/2.0 for panel in panels]
-    print 'X_i points', x_i_points    
+    #print 'X_i points', x_i_points    
     
     plt.scatter(x_i_points[:num_panels/2], C_pi_matrix[:num_panels/2], color = 'red')
-    plt.scatter(x_i_points[num_panels/2:], C_pi_matrix[num_panels/2:], color = 'blue')
+    plt.scatter(x_i_points[num_panels/2:], C_pi_matrix[num_panels/2:], color = 'green')
+    plt.plot(x_i_points[:num_panels/2], C_pi_matrix[:num_panels/2], '-o', color = 'red')
+    plt.plot(x_i_points[num_panels/2:], C_pi_matrix[num_panels/2:], '-o', color = 'green')
     plt.show()
     
     return 0
